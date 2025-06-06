583753fa9d73a142935ead16fae45856743c32b0
Eby Stewart <ebyystewart@gmail.com>
2025-06-06 18:07:11 +0530

    Daily Commit: Helper functions for logging/trace impelemented

---
+++ tcp_ip_trace.c
@@ -0,0 +1,159 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include "tcp_public.h"
+
+static char string_buffer[32];
+
+static void init_string_buffer(void){
+    memset(string_buffer, 0, sizeof(string_buffer));
+}
+
+static char *string_arp_hdr_type(int type)
+{
+    init_string_buffer();
+    switch(type){
+        case ARP_BROAD_REQ:
+        {
+            strncpy(string_buffer, "ARP_BROAD_REQ", strlen("ARP_BROAD_REQ"));
+            break;
+        }
+        case ARP_REPLY:
+        {
+            strncpy(string_buffer, "ARP_REPLY", strlen("ARP_REPLY"));
+            break;
+        }
+        default:
+            ;
+    }
+    return string_buffer;
+}
+
+static char *string_ip_hdr_protocol_val(uint8_t type)
+{
+    init_string_buffer();
+    switch(type){
+        case ICMP_PRO:
+        {
+            strncpy(string_buffer, "ICMP_PRO", strlen("ICMP_PRO"));
+            break;
+        }
+        default:
+            return NULL;
+    }
+    return string_buffer;
+}
+
+static int tcp_dump_appln_hdr_protocol_icmp(char *buff, char *appln_data, uint32_t pkt_size)
+{
+
+    return 0;
+}
+
+static int tcp_dump_ip_hdr(char *buff, ip_hdr_t *ip_hdr, uint32_t pkt_size)
+{
+    int rc = 0;
+    char ip1[16];
+    char ip2[16];
+
+    ip_addr_n_to_p(ip_hdr->src_ip, ip1);
+    ip_addr_n_to_p(ip_hdr->dst_ip, ip2);
+
+    rc += sprintf(buff + rc, "IP Hdr: ");
+    rc += sprintf(buff + rc, "TL: %dB PRO: %s %s -> %s ttl: %d\n", IP_HDR_LEN_IN_BYTES(ip_hdr), string_ip_hdr_protocol_val(ip_hdr->protocol),\
+                           ip1, ip2, ip_hdr->ttl);
+
+    switch(ip_hdr->protocol){
+        case ICMP_PRO:
+        {
+            rc += tcp_dump_appln_hdr_protocol_icmp(buff + rc, IP_PKT_PAYLOAD_PTR(ip_hdr),IP_PKT_PAYLOAD_SIZE(ip_hdr));
+            break;
+        }
+        default:
+            break;
+    }
+
+    return rc;
+}
+
+static int tcp_dump_arp_hdr(char *buff, arp_packet_t *arp_hdr, uint32_t pkt_size)
+{
+    int rc = 0;
+    char ip1[16];
+    char ip2[16];
+
+    rc += sprintf(buff + rc, "ARP Hdr: ");
+    rc += sprintf(buff + rc, "Arp Type: %s %02x:%02x:%02x:%02x:%02x:%02x -->"\
+                                          "%02x:%02x:%02x:%02x:%02x:%02x %s --> %s\n", string_arp_hdr_type(apr_hdr->op_code),\
+                                        arp_hdr->src_mac.mac_addr[0],   \
+                                        arp_hdr->src_mac.mac_addr[1],   \
+                                        arp_hdr->src_mac.mac_addr[2],   \
+                                        arp_hdr->src_mac.mac_addr[3],   \
+                                        arp_hdr->src_mac.mac_addr[4],   \
+                                        arp_hdr->src_mac.mac_addr[5],   \
+                                        arp_hdr->dst_mac.mac_addr[0],   \
+                                        arp_hdr->dst_mac.mac_addr[1],   \
+                                        arp_hdr->dst_mac.mac_addr[2],   \
+                                        arp_hdr->dst_mac.mac_addr[3],   \
+                                        arp_hdr->dst_mac.mac_addr[4],   \
+                                        arp_hdr->dst_mac.mac_addr[5],   \
+                                        ip_addr_n_to_p(arp_hdr->src_ip, ip1),\
+                                        ip_addr_n_to_p(arp_hdr->dst_ip, ip2));
+
+    return rc;
+}
+
+static int tcp_dump_ethernet_hdr(char *buff, ethernet_frame_t *eth_hdr, uint32_t pkt_size)
+{
+    int rc = 0;
+    vlan_ethernet_frame_t *vlan_eth_hdr = NULL;
+    uint32_t payload_size;
+    unsigned short type;
+
+    payload_size = pkt_size - GET_ETH_HDR_SIZE_EXCL_PAYLOAD(eth_hdr);
+
+    vlan_8021q_hdr_t * vlan_8021q_hdr = is_pkt_vlan_tagged(eth_hdr);
+    if(vlan_8021q_hdr){
+        vlan_eth_hdr = (vlan_ethernet_frame_t *)eth_hdr;
+    }
+
+    type = vlan_8021q_hdr ? vlan_eth_hdr->type : eth_hdr->type;
+
+    rc += sprintf(buff + rc, "Eth Hdr: ");
+    rc += sprintf(buff + rc, "%02x:%02x:%02x:%02x:%02x:%02x -->"
+                             "%02x:%02x:%02x:%02x:%02x:%02x %-4s Vlan: %d PL: %dB\n",
+                            eth_hdr->src_mac.mac_addr[0],
+                            eth_hdr->src_mac.mac_addr[1],
+                            eth_hdr->src_mac.mac_addr[2],
+                            eth_hdr->src_mac.mac_addr[3],
+                            eth_hdr->src_mac.mac_addr[4],
+                            eth_hdr->src_mac.mac_addr[5],
+                        
+                            eth_hdr->dst_mac.mac_addr[0],
+                            eth_hdr->dst_mac.mac_addr[1],
+                            eth_hdr->dst_mac.mac_addr[2],
+                            eth_hdr->dst_mac.mac_addr[3],
+                            eth_hdr->dst_mac.mac_addr[4],
+                            eth_hdr->dst_mac.mac_addr[5],
+                            string_ethernet_hdr_type(type),
+
+                            vlan_8021q_hdr ? GET_802_1Q_VLAN_ID(vlan_8021q_hdr) : 0, payload_size);
+    
+    switch(type){
+        case ETH_IP:
+        {
+            rc += tcp_dump_ip_hdr(buff + rc, (ip_hdr_t *)GET_ETHERNET_HDR_PAYLOAD(eth_hdr), payload_size);
+            break;
+        }
+        case ARP_MSG:
+        {
+            rc += tcp_dump_arp_hdr(buff + rc, (arp_packet_t *)GET_ETHERNET_HDR_PAYLOAD(eth_hdr), payload_size);
+            break;
+        }
+        default:
+            break;
+    }
+
+    return rc;
+}
---
+++ tcp_ip_trace.h
@@ -0,0 +1,5 @@
+#ifndef __TCP_IP_TRACE_H__
+#define __TCP_IP_TRACE_H__
+
+
+#endif
