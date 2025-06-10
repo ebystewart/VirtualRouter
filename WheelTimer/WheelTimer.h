a5af098bff803fe0c4b346d80f6caf769bf55391
Eby Stewart <ebyystewart@gmail.com>
2025-06-10 23:56:48 +0530

    Daily Commit: structures for wheel timer declared

---
+++ WheelTimer/WheelTimer.h
@@ -0,0 +1,56 @@
+#ifndef __WHEEL_TIMER_H__
+#define __WHEEL_TIMER_H__
+
+#include <stdint.h>
+#include <pthread.h>
+#include "../glueThread/glthread.h"
+
+typedef struct _wheel_timer_elem_t wheel_timer_elem_t;
+typedef void (*app_call_back)(void *arg, uint32_t sizeof_arg);
+typedef struct _wheel_timer_t wheel_timer_t;
+
+typedef struct slotlist_{
+    glthread_t slots;
+    pthread_mutex_t slot_mutex;
+}slotlist_t;
+
+typedef enum{
+    WTELEM_CREATE,
+    WTELEM_RESCHED,
+    WTELEM_DELETE,
+    WTELEM_SCHEDULED,
+    WTELEM_UNKNOWN
+}wt_opcode_t;
+
+struct _wheel_timer_elem_t{
+    wt_opcode_t opcode;
+    int time_interval;
+    int new_time_interval;
+    int execute_cycle_no;
+    int slot_no;
+    app_call_back app_callback;
+    void *arg;
+    int arg_size;
+    char is_recurrence;
+    glthread_t glue;
+    slotlist_t slotlist_head;
+    glthread_t reschedule_glue;
+    unsigned int N_scheduled;
+    wheel_timer_t *wt;
+};
+
+GLTHREAD_TO_STRUCT(glthread_to_wt_elem, wheel_timer_elem_t, glue);
+GLTHREAD_TO_STRUCT(glthread_reschedule_glue_to_wt_elem, wheel_timer_elem_t, reschedule_glue);
+
+struct _wheel_timer_t {
+	int current_clock_tic;
+	int clock_tic_interval;
+	int wheel_size;
+	int current_cycle_no;
+	pthread_t wheel_thread;
+    slotlist_t reschd_list;
+    unsigned int no_of_wt_elem;
+    slotlist_t slotlist[0];
+};
+
+#endif
